5e6053c6a06a3d4e6ee52a421f42a65342cf37df
Avi Tretiak <avitretiak@gmail.com>
2024-07-17 07:08:36 -0300

    Meta improvements

--- src/meta.js
+++ src/meta.js
@@ -2,8 +2,8 @@
 // @name        Itaú Total Tarjeta
 // @namespace   Violentmonkey Scripts
 // @match       https://www.itaulink.com.uy/trx/tarjetas/credito/*
+// @updateURL   https://github.com/avitretiak/itau-uy-utils/releases/latest/itau-uy-card-utils.user.js
 // @grant       none
-// @version     1.3
 // @author      Avi
 // @description Calculates totals for Itaú credit card transactions
 // ==/UserScript==
